f8bd66156ccef592d6b8023dfebb5025b8196856
Woloda <dorozovecvoloda@gmail.com>
2020-11-26 22:34:53 +0200

    Lab__8_1_3

---
+++ UnitTest(Lab_8.1(3))/UnitTest(Lab_8.1(3)).cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1(3)/Lab_8.1(3).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab813
+{
+	TEST_CLASS(UnitTestLab813)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char lit_r[50] = "QhkiQQSuySQQsdQSdsSQsdQQQSSSq";
+			char para[] = "SQ";
+			char para1[] = "QS";
+			char zamina[] = "***";
+
+			perevirka(lit_r, para, para1, zamina);
+			Assert::AreEqual(lit_r[5], '*');
+		}
+	};
+}
